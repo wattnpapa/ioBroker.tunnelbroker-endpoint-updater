63ba6e403066ed47d8d445ac5fec8285430c30c9
Johannes Rudolph <johannes.rudolph@gmx.com>
2019-04-08 21:51:43 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,58 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+
+    "tunnelproker-endpoint-updater adapter settings": {
+        "en": "Adapter settings for tunnelproker-endpoint-updater",
+        "de": "Adaptereinstellungen für tunnelproker-endpoint-updater",
+        "ru": "Настройки адаптера для tunnelproker-endpoint-updater",
+        "pt": "Configurações do adaptador para tunnelproker-endpoint-updater",
+        "nl": "Adapterinstellingen voor tunnelproker-endpoint-updater",
+        "fr": "Paramètres d'adaptateur pour tunnelproker-endpoint-updater",
+        "it": "Impostazioni dell'adattatore per tunnelproker-endpoint-updater",
+        "es": "Ajustes del adaptador para tunnelproker-endpoint-updater",
+        "pl": "Ustawienia adaptera dla tunnelproker-endpoint-updater",
+        "zh-cn": "tunnelproker-endpoint-updater的适配器设置"
+    },
+
+    "tunnelbroker.net Username": {
+        "en": "tunnelbroker.net Username",
+        "de": "tunnelbroker.net Benutzername",
+        "ru": "tunnelbroker.net Имя пользователя",
+        "pt": "Nome de Usuário do tunnelbroker.net",
+        "nl": "tunnelbroker.net gebruikersnaam",
+        "fr": "tunnelbroker.net Nom d'utilisateur",
+        "it": "tunnelbroker.net Nome utente",
+        "es": "Nombre de usuario de tunnelbroker.net",
+        "pl": "tunnelbroker.net Nazwa użytkownika",
+        "zh-cn": "tunnelbroker.net用户名"
+    },
+
+    "Update Key": {
+        "en": "Update Key",
+        "de": "Schlüssel aktualisieren",
+        "ru": "Обновить ключ",
+        "pt": "Atualizar chave",
+        "nl": "Update sleutel",
+        "fr": "Mettre à jour la clé",
+        "it": "Chiave di aggiornamento",
+        "es": "Clave de actualización",
+        "pl": "Zaktualizuj klucz",
+        "zh-cn": "更新密钥"
+    },
+
+    "Tunnel ID": {
+        "en": "Tunnel ID",
+        "de": "Tunnel-ID",
+        "ru": "Туннель ID",
+        "pt": "ID do túnel",
+        "nl": "Tunnel-ID",
+        "fr": "ID de tunnel",
+        "it": "ID del tunnel",
+        "es": "ID de túnel",
+        "pl": "ID tunelu",
+        "zh-cn": "隧道ID"
+    }
+
+};
